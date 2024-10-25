b70925ce505590edffb76f1f1eb2853fc2cef3a7
Mitchell Barry <mitch.barry@gmail.com>
2024-10-25 03:24:40 +0000

    Added memgraph aspire hosting package

--- src/MemGraphDemo.AppHost/Program.cs
+++ src/MemGraphDemo.AppHost/Program.cs
@@ -1,12 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var memgraph = builder.AddMemgraph("memgraph")
+    .WithMemgraphLab();
+
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.MemGraphDemo_ApiService>("apiservice");
+var apiService = builder.AddProject<Projects.MemGraphDemo_ApiService>("apiservice")
+    .WithReference(memgraph);
 
 builder.AddProject<Projects.MemGraphDemo_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WithReference(apiService);
-    
+
 builder.Build().Run();
